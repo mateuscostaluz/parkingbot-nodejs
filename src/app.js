f4f0048e25ab169f86c9ba15087dc61db8afe0ef
Augusto Santos <augustodossantos.ti@gmail.com>
2019-04-22 21:35:43 -0300

    Commit inicial

---
+++ src/app.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const Telegram = require('telegram-node-bot');
+const TelegramBaseController = Telegram.TelegramBaseController;
+const TextCommand = Telegram.TextCommand;
+const chatbot = new Telegram.Telegram('716039964:AAFsLCI23Ieb3anwXEMhCmoAwgqge95MvlI');
+const request = require('request');
+
+class EventsController extends TelegramBaseController {
+  vagasAction(scope) {
+    let body = vagas();
+    let msg = `No momento existem ` && body && ' vagas disponíveis.';
+    scope.sendMessage(msg);
+  }
+
+  get routes() {
+    return {
+      vagas: 'vagasAction'
+    };
+  }
+
+  get vagas() {
+    request('http://localhost:8080', { json: true }, (err, res, body) => {
+      if (err) {
+        return console.log(err);
+      }
+      return body;
+    });
+  }
+}
+
+chatbot.router.when(new TextCommand('/vagas', 'vagas'), new EventsController());
